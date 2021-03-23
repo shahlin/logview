cf92ff9911138aafc663e30476bd0e5f96bc95ed
Shahlin Ibrahim <shahlin44@gmail.com>
2021-03-23 23:46:10 +0400

    Updated service versions; Added Laravel Horizon; Updated <hr> in template

--- config/services.php
+++ config/services.php
@@ -12,19 +12,31 @@ return [
         'name' => 'Laravel',
         'url' => 'https://raw.githubusercontent.com/laravel/laravel/' . Service::VERSION_EXPR .'/CHANGELOG.md',
         'versions' => [
-            '5.6',
-            '5.7',
-            '5.8',
+            '5.4', '5.5', '5.6', '5.7', '5.8',
+            '6.x', '7.x', '8.x',
         ]
     ],
 
     'react' => [
         'name' => 'React',
-        'url' => 'https://raw.githubusercontent.com/facebook/react/' . Service::VERSION_EXPR . '-stable/CHANGELOG.md',
+        'url' => 'https://raw.githubusercontent.com/facebook/react/' . Service::VERSION_EXPR . '/CHANGELOG.md',
         'versions' => [
-            '0.4',
-            '0.5',
-            '0.8',
+            '0.4-stable', '0.5-stable', '0.8-stable', '0.9-stable',
+            '0.10-stable', '0.11-stable', '0.12-stable', '0.13-stable', '0.14-stable',
+            '15-stable',
+            '16.8.3', '16.8.4', '16.8.5', '16.8.6',
+            '17.0.1', '17.0.2',
+        ]
+    ],
+
+    'laravel-horizon' => [
+        'name' => 'Laravel Horizon',
+        'url' => 'https://raw.githubusercontent.com/laravel/horizon/' . Service::VERSION_EXPR . '/CHANGELOG.md',
+        'versions' => [
+            '2.0',
+            '3.0',
+            '4.x',
+            '5.x',
         ]
     ]
 ];
